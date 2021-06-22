16c3007ba12efbd689f26be7cbb9f78f47c473eb
danny mwarabu <mwarabudanny@gmail.com>
2021-06-22 22:35:23 +0200

    ljgou

---
+++ models/site_db/all_sites.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose')
+var site_structure={ _id:mongoose.Schema.Types.ObjectId,
+                       id:{type: String, required: true,unique: true},
+                       name:{type: String, required: true,unique: true},
+                       site_address:String,
+                       region:String,
+                       town:String,
+                       lattiude:String,
+                       longitude:String,
+                       type:String,
+                       active:{type: String,required: false,default:1}}
+const site_Schema=mongoose.Schema(site_structure) ; 
+module.exports=mongoose.model('all_site',site_Schema);
