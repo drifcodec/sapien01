4a4fd015bfbad15b85f629a741308f135ca19eca
danny mwarabu <mwarabudanny@gmail.com>
2021-07-20 14:01:20 +0200

    vvxv'

--- middleware/user-auth.js
+++ middleware/user-auth.js
@@ -2,14 +2,14 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        console.log("token :"+token)
+        console.log("token :" + token)
         const verified_token = jwt.verify(token, "secret");
-        console.log("verified_token: "+verified_token)
-        req.userData = verified_token; 
+        console.log("verified_token: " + verified_token)
+        req.userData = verified_token;
         next();
     } catch (error) {
         return res.status(401).json({
             message: 'Auth failed error 401 expired token'
         });
     }
-}; 
+};
