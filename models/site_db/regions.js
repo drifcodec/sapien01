c8f302ccc8a0e1021ed89958cfb0ee39dbc4dc67
danny mwarabu <mwarabudanny@gmail.com>
2021-07-23 23:21:42 +0200

    ;lfjeojfepofef

--- models/site_db/regions.js
+++ models/site_db/regions.js
@@ -1,15 +1,8 @@
 const mongoose = require('mongoose')
-var site_structure = {
+var region_structure = {
     _id: mongoose.Schema.Types.ObjectId,
-    site_id: { type: String, required: true, unique: true },
     name: { type: String, required: true, unique: true },
-    site_address: String,
-    region: String,
-    town: String,
-    lattiude: String,
-    longitude: String,
-    site_type: String,
-    active: { type: String, required: false, default: 1 }
+    fullname: { type: String, required: true, unique: true },
 }
-const site_Schema = mongoose.Schema(site_structure);
-module.exports = mongoose.model('all_site', site_Schema);
+const region_Schema = mongoose.Schema(region_structure);
+module.exports = mongoose.model('region', region_Schema);
