bcee5f35398bbad8ecd5d5d0c08bedffb464c60e
danny mwarabu <mwarabudanny@gmail.com>
2021-07-23 18:23:38 +0200

    kjgkj

--- routes/rollout.js
+++ routes/rollout.js
@@ -4,7 +4,7 @@ const roll_out = require("../controllers/roll_out_ctrl/roll_out");
 const category = require("../controllers/roll_out_ctrl/category");
 const sub_category = require("../controllers/roll_out_ctrl/sub_category");
 
-//roll out
+//roll 
 router.post("/roll_out_create", roll_out.roll_out_create)
 router.get("/roll_out_get/:id", roll_out.roll_out_get)
 router.get("/roll_out_distinct_List", roll_out.roll_out_distinct_List)
@@ -12,14 +12,16 @@ router.post("/roll_out_getList", roll_out.roll_out_getList)
 router.post("/roll_out_getList_table", roll_out.roll_out_getList)
 router.patch('/roll_out_update/:id', roll_out.roll_out_update);
 router.delete("/roll_out_delete/:id", roll_out.roll_out_delete)
-    //category
+
+//category
 router.post("/category_create", category.category_create)
 router.get("/category_get/:id", category.category_get)
 router.post("/category_getList", category.category_getList)
 router.post("/category_getList_table", category.category_getList_table)
 router.patch('/category_update/:id', category.category_update);
 router.delete("/category_delete/:id", category.category_delete)
-    //sub_category
+
+//sub_category
 router.post("/sub_category_create", sub_category.sub_category_create)
 router.get("/sub_category_get/:id", sub_category.sub_category_get)
 router.post("/sub_category_getList", sub_category.sub_category_getList)
