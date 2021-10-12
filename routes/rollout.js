c315c7678ae09fc0d9607c81c26aa8dcb9000739
danny mwarabu <mwarabudanny@gmail.com>
2021-10-12 21:40:16 +0200

    fdfdf

--- routes/rollout.js
+++ routes/rollout.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const roll_out = require("../controllers/roll_out_ctrl/roll_out");
 const category = require("../controllers/roll_out_ctrl/category");
 const sub_category = require("../controllers/roll_out_ctrl/sub_category");
+const tokenAuth = require('../middleware/user-auth');
+const userAuth = require('../middleware/roles-auth');
 
 //roll out
 router.post("/roll_out_create", roll_out.roll_out_create)
@@ -10,7 +12,7 @@ router.get("/roll_out_get/:id", roll_out.roll_out_get)
 router.get("/roll_out_distinct_List", roll_out.roll_out_distinct_List)
 router.get("/roll_out_getList", roll_out.roll_out_getList)
 router.get("/roll_out_getList_operator", roll_out.roll_out_getList_operator)
-router.post("/roll_out_getList_table", roll_out.roll_out_getList_table)
+router.post("/roll_out_getList_table",tokenAuth,userAuth.isAllowed,roll_out.roll_out_getList_table)
 router.patch('/roll_out_update/:id', roll_out.roll_out_update);
 router.delete("/roll_out_delete/:id", roll_out.roll_out_delete)
 
