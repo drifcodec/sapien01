4432748336eaf0e8f9c344b2e4d0a33aa32c5cf3
danny <driftcodec@gmail.com>
2023-09-07 14:32:06 +0200

    changed text

--- appsServer/user_management/routes/user_roles.js
+++ appsServer/user_management/routes/user_roles.js
@@ -5,7 +5,7 @@ const tokenAuth = require('../../../middleware/user-auth');
 const userAuth = require('../../../middleware/roles-auth');
 
 router.post("/user_role_create", role.create)
-//srouter.put('/user_role_update/:id', role.update);
+//srouter.put('/user_role_update/:id', role.update) ;
 router.post("/user_role_getList_table/", role.getList_table)
 router.get("/user_role_getList", role.getList)
 router.get("/user_role_get/:id", role.get)
